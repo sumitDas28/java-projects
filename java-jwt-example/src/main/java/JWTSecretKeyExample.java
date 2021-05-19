7c711435f5d30a330236b93136363f7529ce5703
sumitdas <1.sumitdas@gamil.com>
2021-05-19 11:27:08 -0400

    Initial commit : JWT Secret Key Example

---
+++ java-jwt-example/src/main/java/JWTSecretKeyExample.java
@@ -0,0 +1,34 @@
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Random;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class JWTSecretKeyExample {
+
+	public static void main(String[] args) {
+		
+		Instant now = Instant.now();
+		
+		byte[] secretKey= Base64.getDecoder().decode("cmbLuc7cM+9CPNZs610knGvKuXp4ayGnRfHljvdHvww=");
+		
+		System.out.println("secretKey: "+secretKey);
+		
+		String jwt = Jwts.builder()
+				.setSubject("Sumit Das")
+				.setAudience("View Demo")
+				.claim("id20", new Random().nextInt(20)+1)
+				.setIssuedAt(Date.from(now))
+				.setExpiration(Date.from(now.plus(1, ChronoUnit.MINUTES)))
+				.signWith(Keys.hmacShaKeyFor(secretKey))
+				.compact();
+		
+		System.out.println("jwt: "+jwt);
+				
+
+	}
+
+}
